94907002079ed073c4769d869d4e0b2d2485a25b
M4tthewDE <mattider2@gmail.com>
2021-09-23 09:34:41 +0200

    add assembler opening input asm file

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,12 +1,20 @@
+use std::fs::File;
+
 #[derive(Debug)]
 pub struct Assembler{
-    input_asm: String,
+    input_asm: File,
 }
 
 impl Assembler {
-    pub fn new(input_asm: String) -> Assembler {
+    pub fn new(input_asm_name: String) -> Assembler {
+        let file = File::open(input_asm_name);
+        let file = match file {
+            Ok(file) => file,
+            Err(error) => panic!("Problem opening file: {:?}", error),
+        };
+        
         Assembler {
-            input_asm: input_asm,
+            input_asm: file,
         }
     }
 }
