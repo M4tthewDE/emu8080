ec8afd1837bf349aed389a77ee68385ae6a249d9
M4tthewDE <mattider2@gmail.com>
2021-09-26 19:52:15 +0200

    add ADD instruction to cpu

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use assembler::{Instruction, InstructionCommand, InstructionArgument};
 
 fn main() {
     let mut cpu = initialize_cpu();
+
+    // set A to 12, to see effects of assembly code execution
+    cpu.set_register(0, initialize_register(12));
     
     let assembler = assembler::Assembler::new("test.asm".to_owned(), "output".to_owned());
 
@@ -49,7 +52,7 @@ impl Cpu {
     }
 
     fn run(&mut self, instructions: &Vec<Instruction>) {
-        // initial status
+        println!("Initial status:");
         self.get_status();
 
         for instruction in instructions {
@@ -64,8 +67,17 @@ impl Cpu {
     fn execute(&mut self, instruction: &Instruction) {
         match instruction.command {
             InstructionCommand::MOV => self.execute_mov(&instruction.arguments),
+            InstructionCommand::ADD => self.execute_add(&instruction.arguments),
             _ => panic!("Invalid command!")
         }        
+    }
+
+    fn execute_add(&mut self, args: &Vec<InstructionArgument>) {
+        let source_register = self.get_register(args[0].to_index().into());        
+        let current_a = self.get_register(0).value;
+
+        let added_register = Register {value: current_a+source_register.value};
+        self.set_register(0, added_register);
     }
 
     fn execute_mov(&mut self, args: &Vec<InstructionArgument>) {
