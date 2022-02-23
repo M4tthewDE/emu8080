383e05fb7cd9cdf4927e69a4b5e5f37c254c59fb
M4tthewDE <mattider2@gmail.com>
2022-02-23 09:00:17 +0100

    JC instruction (#78)
    * Add JC instruction to cpu
    
    * Add instruction to assembler pipeline
    
    * Check off instruction in readme
    
    * Execute jc instruction in cpu

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -293,6 +293,7 @@ impl Cpu {
     fn execute_label_instruction(&mut self, command: &InstructionCommand, address: u16) {
         match command {
             InstructionCommand::Jmp => self.execute_jmp(address),
+            InstructionCommand::Jc => self.execute_jc(address),
             _ => panic!("invalid instruction"),
         }
     }
@@ -1091,6 +1092,12 @@ impl Cpu {
         self.set_program_counter(address);
     }
 
+    fn execute_jc(&mut self, address: u16) {
+        if self.get_flag(Flag::C) {
+            self.set_program_counter(address);
+        }
+    }
+
     fn print_status(&self) {
         for i in 0..7 {
             println!(
@@ -1169,7 +1176,7 @@ mod tests {
         assert_eq!(cpu.get_memory(42), 127);
         assert_eq!(cpu.get_memory(12345), -1);
         assert_eq!(cpu.get_memory(12346), 27);
-        assert_eq!(cpu.get_program_counter(), 68);
+        assert_eq!(cpu.get_program_counter(), 71);
     }
 
     #[test]
@@ -2029,6 +2036,20 @@ mod tests {
         cpu.set_program_counter(10);
         cpu.execute_jmp(1234);
 
+        assert_eq!(cpu.get_program_counter(), 1234);
+    }
+
+    #[test]
+    fn test_execute_jc() {
+        let mut cpu = initialize_cpu();
+
+        cpu.set_program_counter(10);
+        cpu.set_flag(Flag::C, false);
+        cpu.execute_jc(1234);
+        assert_eq!(cpu.get_program_counter(), 10);
+
+        cpu.set_flag(Flag::C, true);
+        cpu.execute_jc(1234);
         assert_eq!(cpu.get_program_counter(), 1234);
     }
 
