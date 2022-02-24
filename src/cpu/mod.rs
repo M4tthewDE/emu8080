c3c93dc54823f5a205af4f62c8e6654258c11b93
M4tthewDE <mattider2@gmail.com>
2022-02-24 12:42:20 +0100

    Add JPE instruction to CPU

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1133,6 +1133,12 @@ impl Cpu {
         }
     }
 
+    fn execute_jpe(&mut self, address: u16) {
+        if self.get_flag(Flag::P) {
+            self.set_program_counter(address);
+        }
+    }
+
     fn print_status(&self) {
         for i in 0..7 {
             println!(
@@ -2156,6 +2162,20 @@ mod tests {
 
         cpu.set_flag(Flag::S, false);
         cpu.execute_jp(1234);
+        assert_eq!(cpu.get_program_counter(), 1234);
+    }
+
+    #[test]
+    fn test_execute_jpe() {
+        let mut cpu = initialize_cpu();
+
+        cpu.set_program_counter(10);
+        cpu.set_flag(Flag::P, false);
+        cpu.execute_jpe(1234);
+        assert_eq!(cpu.get_program_counter(), 10);
+
+        cpu.set_flag(Flag::P, true);
+        cpu.execute_jpe(1234);
         assert_eq!(cpu.get_program_counter(), 1234);
     }
 
